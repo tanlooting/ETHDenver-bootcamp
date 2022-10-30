19291d4d8ee01227edf333e003e211643d5936bd
Loo Ting Tan <tanlooting@LT-mbp.local>
2022-10-31 02:10:03 +0800

    added HW8

---
+++ VolcanoCoin/test/VolcanoCoin.t.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+import "ds-test/test.sol";
+import "forge-std/Test.sol";
+import "../src/VolcanoCoin.sol";
+
+contract VolcanoCointest is Test {
+
+    VolcanoCoin public volcanoCoin;
+    address owner;
+
+    function setUp() public {
+        /// runs before every unit test?
+        vm.startPrank(owner);
+        volcanoCoin = new VolcanoCoin();
+       
+    }
+
+    function testInitialSupply() public {
+        /// initial supply has to be 10000
+        uint totalSupply = volcanoCoin.getSupply();
+        assertEq(totalSupply, 10000);
+    }
+
+    function testSupplyIncrement() public {
+        /// supply increment has to be by 1000
+        uint initialSupply = volcanoCoin.getSupply();
+        volcanoCoin.addSupply();
+        uint increment = volcanoCoin.getSupply() - initialSupply;
+        assertEq(increment, 1000);
+    }
+
+    function testFailUserChangeSupply() public {
+        vm.stopPrank();
+        vm.startPrank(address(99));
+        volcanoCoin.addSupply();
+        /// only owner of contract can increase the supply
+    }
+
+    function testExample() public {
+       bool testPassed = true ;// testing 
+       assertTrue(testPassed);
+    }
+
+    function testTransfer() public{
+        volcanoCoin.transfer(100, address(666));
+        assertEq(volcanoCoin.getBalance(address(666)), 100);
+        assertEq(volcanoCoin.getBalance(owner), 9900);
+    
+    }
+
+}
